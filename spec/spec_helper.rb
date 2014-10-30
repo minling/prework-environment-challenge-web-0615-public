dd2cab73dbd5af97be03b2efd29c949b167a3ac1
ahimmelstoss <ahimmelstoss@gmail.com>
2014-10-30 15:35:24 -0400

    remove method

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,14 +2,3 @@ require 'open3'
 
 RSpec.configure do |config|
 end
-
-# def capture_stdout(&block)
-#   original_stdout = $stdout
-#   $stdout = fake = StringIO.new
-#   begin
-#     yield
-#   ensure
-#     $stdout = original_stdout
-#   end
-#   fake.string
-# end
