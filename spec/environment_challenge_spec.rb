c5fab192738e1f68a77ada73e3ec13eb7ef7e90e
ahimmelstoss <ahimmelstoss@gmail.com>
2014-10-30 15:35:00 -0400

    finished

--- spec/environment_challenge_spec.rb
+++ spec/environment_challenge_spec.rb
@@ -1,20 +1,21 @@
 describe "checking our environment setup" do
   it 'has gcc installed' do 
-    # output = capture_stdout { `gcc` }
-    # expect(output).to match(/clang: error: no input files/)
-
-    expect(system("gcc")).to eq(true)
+    expect(Open3.capture3("gcc")[1]).to eq("clang: error: no input files\n")
   end
 
-  xit 'has homebrew installed' do 
+  it 'has homebrew installed' do 
+    expect(Open3.capture3("brew doctor")[0]).to include("Homebrew")
   end
 
-  xit 'has git installed' do 
+  it 'has git installed' do 
+    expect(Open3.capture3("git")[0]).to include("usage: git")
   end
 
-  xit 'has rvm installed' do 
+  it 'has rvm installed' do 
+    expect(Open3.capture3("which ruby")[0]).to include(".rvm")
   end
 
-  xit 'has ironboard gem installed' do 
+  it 'has ironboard gem installed' do 
+    expect(Open3.capture3("gem list")[0]).to include("rspec-ironboard")
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,15 @@
+require 'open3'
+
 RSpec.configure do |config|
 end
 
-def capture_stdout(&block)
-  original_stdout = $stdout
-  $stdout = fake = StringIO.new
-  begin
-    yield
-  ensure
-    $stdout = original_stdout
-  end
-  fake.string
-end
+# def capture_stdout(&block)
+#   original_stdout = $stdout
+#   $stdout = fake = StringIO.new
+#   begin
+#     yield
+#   ensure
+#     $stdout = original_stdout
+#   end
+#   fake.string
+# end
