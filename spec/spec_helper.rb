2efdd36e60c054e3c0bae67ed3893d6e73aaa3b2
changamanda <amanda.chang10@gmail.com>
2015-01-22 14:43:58 -0500

    Name new file depending on pass or failure

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,18 @@
 require 'open3'
 
-File.open("environment-file.txt", "w") {|f| f.write("I ran tests, and am a programming rockstar!") }
-
 RSpec.configure do |config|
+	File.delete('environment-check-passed.txt') if File.exist?('environment-check-passed.txt')
+	File.delete('environment-check-failed.txt') if File.exist?('environment-check-failed.txt')
+	
+	errors = []
+	config.after(:each){ |example| errors << example.exception }
+	config.after(:all) do
+		new_filename = "environment-check-failed.txt"
+		message = "Try again!"
+		if errors.compact.empty?
+			new_filename = "environment-check-passed.txt"
+			message = "I passed the tests, and am a programming rockstar!"
+		end
+		File.open(new_filename, "w") {|f| f.write(message) }
+	end
 end
