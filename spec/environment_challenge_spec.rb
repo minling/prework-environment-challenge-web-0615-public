ec464bfa294aa6f68b479b116d9058c508175a02
Ian Miller <irmiller22@gmail.com>
2015-02-24 13:09:31 -0500

    fixed environment challenge spec test

--- spec/environment_challenge_spec.rb
+++ spec/environment_challenge_spec.rb
@@ -1,21 +1,21 @@
 describe "checking our environment setup" do
-  it 'has gcc installed' do 
+  it 'has gcc installed' do
     expect(Open3.capture3("gcc")[1]).to eq("clang: error: no input files\n")
   end
 
-  it 'has homebrew installed' do 
-    expect(Open3.capture3("brew doctor")[0]).to include("Homebrew")
+  it 'has homebrew installed' do
+    expect(Open3.capture3("brew doctor")[0]).to include("Homebrew").or include("Your system is ready to brew.")
   end
 
-  it 'has git installed' do 
+  it 'has git installed' do
     expect(Open3.capture3("git")[0]).to include("usage: git")
   end
 
-  it 'has rvm installed' do 
+  it 'has rvm installed' do
     expect(Open3.capture3("which ruby")[0]).to include(".rvm")
   end
 
-  it 'has ironboard gem installed' do 
+  it 'has ironboard gem installed' do
     expect(Open3.capture3("gem list")[0]).to include("ironboard")
   end
 end
